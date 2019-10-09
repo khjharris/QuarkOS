6a2b6bdb5f1ec1fa280da784a924af79fc6cf3a6
nomal <malhin@bu.edu>
2019-10-08 23:39:27 -0400

    Fixed vector table interrupt values, began translating start routines for hypervisor

--- hypervisor/init.asm
+++ hypervisor/init.asm
@@ -3,38 +3,43 @@
 	*Author: Noah malhi - malhin@bu.edu
 **/
 
-global _start
+global _main
 _main:
-	b	 _init
-
-	.macro	 entry tag
-		.align		7
-		b	/tag	
-	.endm
-	.align	11
-	.global vector_table:
-	
-vector_table:
+	jmp		_init
+//may need to setup alignment --> believe they start at 0x00
+_vector_table:
+	//based on page 457 of Quakc1000
 	entry _unhandled_vec	//Divide by 0
 	entry _unhandled_vec	//Debug Exception
-	//entry _unhandled_vec	//Reserved
-	entry _unhandled_vec	//Breakpoint (INT3)
-	//entry _unhandled_vec	//Reserved
-	//entry _unhandled_vec	//Reserved		
-	entry _unhandled_vec	//Invalid opcode
-	//entry _unhandled_vec	//reserved
+	entry _unhandled_vec	//NMI Interrupt
+	entry _unhandled_vec	//Breakpoint 
+	entry _unhandled_vec	//overflow
+	entry _unhandled_vec	//bound range exceeded 		
+	entry _unhandled_vec	//Invalid opcod e
+	entry _unhandled_vec	//Device not available
 	entry _unhandled_vec	//Double fault
-	//entry _unhandled_vec	//reserved
-	//entry _unhandled_vec	//Reserved
-	entry _unhandled_vec	//not Present
-	//entry _unhandled_vec  //reserved
+	entry _unhandled_vec	//Intel reserved
+	entry _unhandled_vec	//Invalid TSS
+	entry _unhandled_vec	//Segment not present
+	entry _unhandled_vec    //Stack segment fault
 	entry _unhandled_vec	//General protection
-	//entry _unhandled_vec	//reserved
-	//entry _unhandled_vec	//Reserved
-	entry _unhandled_vec	//machine check
-	//entry _unhandled_vec	//reserved
-	//19-31 Reserved
-	entry _unhandled_vec	//asychronous IRQ, 320255
-	
+	entry _unhandled_vec	//Page fault
+	entry _unhandled_vec	//Intel reserved
+	entry _unhandled_vec	//floating point error
+	entry _unhandled_vec	//alignment check
+	//18-31 Intel Reserved
+	//32-255 User Defined interrupts
+
+//dont use %rsp --> includes stack pointer %esp
 _init:
-	mov x2, 
+	//create el2 stack and point to it
+	mov 	$el2_stack_end, %rax
+	mov 	%rax, %esp
+
+	//Clear binaries???
+	
+	//jump to srart routine
+	jmp		_start
+
+//if reaches here in assembly then goes to failsafe
+fail:	jmp		failsafe
